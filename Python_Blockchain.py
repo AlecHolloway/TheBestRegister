16a3e98075f393351114bfc0b781158a9fe67449
chapiiin <32081747+chapiiin@users.noreply.github.com>
2019-11-12 13:29:52 -0600

    Add files via upload

--- Python_Blockchain.py
+++ Python_Blockchain.py
@@ -3,6 +3,7 @@ import hashlib
 from pygrok import Grok
 import time
 
+
 class Block:
     blockNo = 0
     data = None
@@ -11,11 +12,13 @@ class Block:
     nonce = 0
     previous_hash = 0x0
     timestamp = datetime.datetime.now()
+    store_location = "601 Critz Street Starkville, MS"
 
     trans_id = 0
 
     def __init__(self, data):
         self.data = data
+        
 
     def hash(self):
         h = hashlib.sha256()
@@ -30,11 +33,11 @@ class Block:
         return h.hexdigest()
 
     def __str__(self):
-        return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNo) + "\nBlock Data: " + str(self.data) + "\nHashes: " + str(self.nonce) + "\nTimestamp: " + str(self.timestamp) + "\nTransaction ID: " + str(self.trans_id) + "\n--------------"
+        return "Block Hash: " + str(self.hash()) + "\nBlockNo: " + str(self.blockNo) + "\nBlock Data: " + str(self.data) + "\nHashes: " + str(self.nonce) + "\nTimestamp: " + str(self.timestamp) + "\nTransaction ID: " + str(self.trans_id) + "\nStore Location: " + str(self.store_location) + "\n--------------"
 
 class Blockchain:
 
-    diff = 10
+    diff = 20
     maxNonce = 2**32
     target = 2 ** (256-diff)
 
@@ -43,14 +46,13 @@ class Blockchain:
 
     def add(self, block):
         block.trans_id = self.block.trans_id + 1
+        
         block.previous_hash = self.block.hash()
         block.blockNo = self.block.blockNo + 1
 
         self.block.next = block
         self.block = self.block.next
-
-
-        print("---data added---")
+        print("---transaction added---")
 
     def mine(self, block):
         for n in range(self.maxNonce):
@@ -62,57 +64,59 @@ class Blockchain:
                 block.nonce += 1
 
 def main():
-                ##      searching by block number
-                ##      x = int(input("Enter which block's information you would like to display: "))
     block = Block(0)
     blockchain = Blockchain()
     print("Current time:", block.timestamp)
-    
-    
+
     #add data to the blockchain
     blockchain.add(Block("Apples : $4.45"))
-    time.sleep(1)
+    #time.sleep(1)
     blockchain.add(Block("Oranges : $6.54"))
-    time.sleep(1)
+    #time.sleep(1)
     blockchain.add(Block("Pairs: $2.23"))
 
-    #Mine for blocks
-    ##for n in range(5):
-    ##    blockchain.mine(Block("Block " + str(n+1)))
 
-    #Print entire blockchain
-    while blockchain.head != None:
+    #Give choice of searching by date or transaction ID
+    answer = str(input("Would you like to search by date or by transaction ID (enter 'date' or 'id'): "))
+
+    if (answer == "id"):
+        #searching by block number
+        x = int(input("Enter which transaction ID you would like to display: "))
+
+        i = 0
+        #Print the selected block
+        while i < x:
+            blockchain.head = blockchain.head.next
+            i += 1
+        print("Displaying information for the number ", x, " block: ")
         print(blockchain.head)
-        blockchain.head = blockchain.head.next
 
+    elif (answer == "date"):
+        #searching by date
+        #declare variables for start and end date
+        i = 0.0
+        x = str(input("Enter the start date (yyyy-mm-dd): "))
+        y = str(input("Enter the end date (yyyy-mm-dd): "))
 
-                ##    i = 0
-                ##    #Print the selected block
-                ##    while i < x:
-                ##        blockchain.head = blockchain.head.next
-                ##        i += 1
-                ##    print("Displaying information for the number ", x, " block: ")
-                ##    print(blockchain.head)
-
-    #declare variables for start and end date
-    i = 0.0
-    x = str(input("Enter the start date (yyyy-mm-dd): "))
-    y = str(input("Enter the end date (yyyy-mm-dd): "))
-
-    
-    start = datetime.datetime.strptime(x, "%Y-%m-%d")
-    end = datetime.datetime.strptime(y, "%Y-%m-%d")
-    date_array = \
-        (start + datetime.timedelta(days=x) for x in range(0, (end-start).days))
-    
-    #Date of transactions within range entered
-    for date_object in date_array:
-        print(date_object.strftime("%Y-%m-%d"))
         
+        start = datetime.datetime.strptime(x, "%Y-%m-%d")
+        end = datetime.datetime.strptime(y, "%Y-%m-%d")
+        date_array = \
+            (start + datetime.timedelta(days=x) for x in range(0, (end-start).days))
+        
+        #Date of transactions within range entered
+        for date_object in date_array:
+            print(date_object.strftime("%Y-%m-%d"))
+            
 
-    start_date_str = str(blockchain.head.timestamp)
-    start_date = start_date_str[:10]
-    print("Date as a string: ", start_date)
+        start_date_str = str(block.timestamp)
+        start_date = start_date_str[:10]
+        print("Date as a string: ", start_date)
+
+
+
+        print("Displaying information for the purchases made on or after ", x, " and before ", y)
+
 
 ##    #Printing blocks within date range
 ##    for date_object in date_array:
@@ -128,13 +132,18 @@ def main():
 ##        blockchain.head = blockchain.head.next
 ##        i += 1
 
-    print("Displaying information for the purchases made on or after ", x, " and before ", y)
 
     #Print blocks until end date reached
 ##    while i < y:
 ##        print(blockchain.head)
 ##        blockchain.head = blockchain.head.next
-    
+
+
+
+    #Print entire blockchain
+##    while blockchain.head != None:
+##        print(blockchain.head)
+##        blockchain.head = blockchain.head.next
     print()
     print()
     main()
